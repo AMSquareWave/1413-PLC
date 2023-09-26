897b0d53cc81c07d66641a2a5b3394f156236f2c
AMSquareWave <amsquarewave@gmail.com>
2023-09-26 09:48:10 -0500

    Implemented New imageManipulator Function

--- assignments/P02/main.go
+++ assignments/P02/main.go
@@ -1,11 +1,22 @@
 package main
 
-import "P02/imagemod/imageManipulator"
+import (
+	"P02/imagemod/imageManipulator"
+	"fmt"
+)
 
 func main() {
 	im := imageManipulator.NewImageManipulator(800, 600)
+	im2, err := imageManipulator.NewImageManipulatorWithImage("mustangs.jpg")
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
 
 	im.DrawRectangle(420, 69, 42, 42)
+	im2.DrawRectangle(200, 100, 500, 350)
 
 	im.SaveToFile("silly-little-image.png")
+	im2.SaveToFile("mustangs.png")
 }
